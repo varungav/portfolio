837772b404539fdba5807b26c69c9607c18b77ff
Varun Gavoor <varun.gavoor@7edge.com>
2025-11-15 01:57:48 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-center py-8">My Portfolio</h1>
+    </div>
+  )
+}
+
+export default App
